4b34c369e0fe470824b2caf17ad64f14903cb56c
irlbhop <18dickmadugodagesn2@webmail.sis.edu.hk>
2016-10-05 09:59:52 +0800

    Quiz

---
+++ quiz/js/index.js
@@ -0,0 +1,20 @@
+function quiz() {
+  var question1 = prompt("What is the Capital of Portugal?");
+  if (question1 === "Lisbon") {
+    alert("CORRECT!")
+  } else {
+    alert("RIP IN PEP, WRONG!");
+  }
+  var question2 = prompt("What is the Capital of the United States of America?");
+  if (question2 === "Washington D.C") {
+    alert("CORRECT!")
+  } else {
+    alert("RIP IN PEP, WRONG!");
+  }
+  var question3 = prompt("What is the Capital of Saudi Arabia?");
+  if (question3 === "Riyadh") {
+    alert("CORRECT!")
+  } else {
+    alert("RIP IN PEP, WRONG!");
+  }
+}
